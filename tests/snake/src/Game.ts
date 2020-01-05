a0db766e009f382e83d7819ff3efce312e284ebd
Brett McNamara <brettmc@gmail.com>
2020-01-05 18:19:14 -0500

    game

--- tests/snake/src/Game.ts
+++ tests/snake/src/Game.ts
@@ -1,4 +1,13 @@
 import { PixelBox, PixelColor } from "simpleview-lib";
+import { snakeFeed, snakeHead, snakeMove, createSnake, Snake } from "./Snake";
+import { createPoint } from "./Point";
+
+const HeadingDelta = {
+    up: createPoint(0, -1),
+    down: createPoint(0, 1),
+    left: createPoint(-1, 0),
+    right: createPoint(1, 0)
+};
 
 export enum Direction {
     Up, Down, Left, Right
@@ -17,10 +26,14 @@ const COLOR_FOOD: PixelColor = "#f00";
 export const initGame = (view: PixelBox): Game => {
     let done = false;
 
+    let snake = createSnake(HeadingDelta.right, [ createPoint(Math.floor(view.width / 3), Math.floor(view.height / 3))]);
+    snake = snakeMove(snakeFeed(snakeMove(snakeFeed(snakeMove(snakeFeed(snake))))));
+
     const isDone = () => done;
     const inputHandler = (dir: Direction) => { };
     const step = () => {
         view.fill(COLOR_BG);
+        snake.body.forEach(({x, y}) => view.setPixel(x, y, COLOR_SNAKE));
         view.draw();
     };
 
